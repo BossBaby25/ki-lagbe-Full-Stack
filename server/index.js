336f39ea5ca5bfcf93fd68f80b62df772a0a77b8
BossBaby25 <meraj15-4213@diu.edu.bd>
2025-02-22 14:03:47 +0600

    frontend

--- server/index.js
+++ server/index.js
@@ -17,14 +17,14 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use(morgan());
+app.use(morgan("combined"));
 app.use(
   helmet({
     crossOriginResourcePolicy: false,
   })
 );
 
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.get("/", (request, response) => {
   ///server to client
